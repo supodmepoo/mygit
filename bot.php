f94cc393fe054f93b06621187f4494a5529563b9
supodmepoo <supodmepoo@gmail.com>
2017-07-05 13:43:39 +0700

    edit bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,3 @@
-<?php
 curl -X GET \
 -H 'Authorization: Bearer YVslD/pVj14UB9MtS3cyDVeb8UU1/ThStOB9l1iAM81yBkMkns+zta84Hq179vbqzft0WjPmVRrZa/lnZyR/k3iU+mOqOvwNirBFEptNQHbkWpn+X4l0xgnT4z3z0hZlcf1ZOOceaKikHVdjzIQJ5QdB04t89/1O/w1cDnyilFU=' \
 https://api.line.me/v1/oauth/verify
-?> 
